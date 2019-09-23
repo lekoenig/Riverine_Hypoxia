2b6e4d64a097c4e0005db36b36f9ed4b913e5b3b
Lauren Koenig <lrnkoenig@gmail.com>
2019-09-23 16:10:26 -0400

    Edited 01_Prep_spatial_data.R to output flagged hypoxia locations.

--- 01_Prep_spatial_data.R
+++ 01_Prep_spatial_data.R
@@ -5,12 +5,12 @@
 ## The objective of this script is to import the global hypoxia dataset for formatting and subsetting for further spatial analysis
 
 # Load packages:
-library(dplyr)         # general data cleaning/aggregating
-library(sf)            # used for geospatial analyses
-library(mapview)       # plot spatial objects
+library(dplyr)           # general data cleaning/aggregating
+library(sf)              # used for geospatial analyses
+library(mapview)         # plot spatial objects
 #devtools::install_github("USGS-R/nhdplusTools")  # install Blodgett nhdplusTools ()
-library(nhdplusTools)  # USGS OWI package for interfacing with NHDV2 and NHDHR
-library(USAboundaries)
+library(nhdplusTools)    # USGS OWI package for interfacing with NHDV2 and NHDHR
+library(USAboundaries)   # package for interfacing with USA boundary spatial data
 
 
 ## --------------- Import data --------------- ##
@@ -69,29 +69,30 @@ library(USAboundaries)
   ## check that crs reflects WGS84 and plot data (note that for now this is ignoring sites where coord_units was equal to UNKWN/NA/OTHER/OLHI/Unknown (approx. 15% of the dataset):
   st_crs(hypoxia.dat2)
   mapview(hypoxia.dat2)
+  
+  ## rename column X to represent unique database ID:
+  names(hypoxia.dat2)[names(hypoxia.dat2) == 'X'] <- 'db_ID'
     
   ## write ESRI shapefile and save data to output folder (optional):
   #st_write(hypoxia.dat2, "./output/spatial/Hypoxia_dat_20190715.shp")
   saveRDS(hypoxia.dat2, "./output/Hypoxia_dat_20190812.rds")
 
     
-## ------------ Subset U.S. sites and link to NHD reaches ------------ ##
+## ------------ Subset U.S. sites  ------------ ##
   
   # 1. Clip hypoxia dataset to USA extent
   
   # Define bounding box for continental United States (limited by spatial extent of NHDV2, which does not include AK, combined with StreamCat, which does not include HI or PR):
-  #usa.extent <- st_read("./data/spatial/usa_extent_albers.shp",crs=102003)
   usa.extent <- USAboundaries::us_boundaries(type="state", resolution = "low") %>% 
                       filter(!state_abbr %in% c("PR", "AK", "HI")) %>%
                       st_combine(.) %>%
                       # project data to albers equal area:
                       st_transform(crs = 5070)
                 
-                 
   # Clip hypoxia dataset to spatial extent of continental United States:
   hypoxia.dat2.usa <- hypoxia.dat2 %>%
-                      mutate(Lat = st_coordinates(.)[,2],
-                             Lon = st_coordinates(.)[,1]) %>%
+                      mutate(Latitude = st_coordinates(.)[,2],
+                             Longitude = st_coordinates(.)[,1]) %>%
                       # project data to albers equal area:
                       st_transform(crs=5070) %>%
                       # subset by usa extent:
@@ -101,13 +102,84 @@ library(USAboundaries)
   mapview(hypoxia.dat2.usa) 
   st_crs(hypoxia.dat2.usa)
     
-  # rename column X to represent unique database ID:
-  names(hypoxia.dat2.usa)[names(hypoxia.dat2.usa) == 'X'] <- 'db_ID'
-  
   # save data to output folder (optional):
   saveRDS(hypoxia.dat2.usa, "./output/Hypoxia_dat_USA_20190812.rds")            # save as RDS file
   st_write(hypoxia.dat2.usa, "./output/spatial/Hypoxia_dat_usa_20190812.shp")   # save as ESRI shapefile
   
+
+## ------------ Identify flagged sites (to double-check lat/long or native crs)  ------------ ##
   
+  # sites with unknown crs:
+    names(hypoxia.dat)[names(hypoxia.dat) == 'X'] <- 'db_ID'
+    crs.flag <- hypoxia.dat %>%
+                filter(is.na(Coord_Units) | Coord_Units == "UNKWN" | Coord_Units == "OTHER" | 
+                       Coord_Units == "OLDHI" | Coord_Units == "Unknown" | Coord_Units == "") 
+    crs.flag$NOTES <- "Unknown or unrecognized coordinate reference system"
+    crs.flag$Coord_Units_new <- "Unknown"
+    
+  # sites that have NA for lat/lon or that fall outside of expected bounds for lat/lon (e.g. https://spatialreference.org/ref/epsg/wgs-84/)
+    wgs84.flag1 <- hypoxia.dat[which(hypoxia.dat$Coord_Units=="WGS84"),] %>%
+                  filter(Latitude < - 90 | Latitude > 90 | Longitude < -180 | Longitude > 180)
+    wgs84.flag1$NOTES <- "lat/lon is outside of expected bounds"
+    wgs84.flag2 <- hypoxia.dat[which(hypoxia.dat$Coord_Units=="WGS84"),] %>%
+                  filter(is.na(Latitude))
+    wgs84.flag2$NOTES <- "lat/lon is NA"
+    wgs84.flag <- rbind(wgs84.flag1,wgs84.flag2)
+    wgs84.flag$Coord_Units_new <- "WGS84"
+    
+  # sites in USA that fall outside of HUC2 regions
+    nhd.region <- read_sf(dsn="./data/spatial/",
+                          layer="USA_HydroRegions_VPU02")
+    nhd.region <- nhd.region[-which(is.na(nhd.region$VPUID)|nhd.region$VPUID=="20"|nhd.region$VPUID=="21"|nhd.region$VPUID=="22"),] %>%
+      st_transform(5070)
+    
+    USA.in <- hypoxia.dat2.usa %>% 
+                   .[nhd.region,] # subsetting + dataframe anti_join seems faster than st_disjoint to get USA.out
+    USA.out <- anti_join(st_drop_geometry(hypoxia.dat2.usa),st_drop_geometry(USA.in),by="db_ID")
+    USA.out.spatial <- hypoxia.dat2.usa[which(hypoxia.dat2.usa$db_ID %in% USA.out$db_ID),]
+    #mapview(nhd.region) + mapview(hypoxia.dat2.usa) + mapview(USA.out.spatial,col.region="red")
+    USA.out$NOTES <- "sites not contained within USA HUC2 regions"
+    USA.out$Coord_Units_new <- "WGS84"
+    
+  # sites that are plotting off of the continents:
+   
+    # Read in polygon shapefile of world ocean extent:
+    oceans <- st_read(dsn= "./data/spatial/",layer="ne_10m_ocean")
+      
+    crs = 54012   # use world eckert IV equal area projection to compromise distortion equally
+    ocean.proj <- st_transform(oceans, crs)
+      
+    # Identify which continent overlaps each sample point:
+    hypoxia.dat2$continent <- NA
+    for(i in 1:length(hypoxia.dat2$db_ID)){
+      pt = hypoxia.dat2[i,]
+      pt.proj <- st_transform(pt,crs)
+      pt.buffer <- st_buffer(pt.proj,5000)
+        
+      #pt.int <- st_intersects(pt.proj,ocean.proj,sparse=FALSE)
+      pt.int <- st_covers(ocean.proj,pt.buffer,sparse=FALSE)
+        
+      if("TRUE" %in% pt.int){
+        continent <- "ocean"
+      } else {
+        continent <- "land_surface"
+      }
+      hypoxia.dat2$continent[i] <- continent
+      rm(continent)
+      print(i)
+      }
+      
+    ocean.flag <- hypoxia.dat2[which(hypoxia.dat2$continent=="ocean"),] %>%
+                  mutate(Latitude = st_coordinates(.)[,2],
+                         Longitude = st_coordinates(.)[,1]) %>%
+                  st_drop_geometry(.)
+    ocean.flag$NOTES <- "site plotting in ocean"
+    ocean.flag <- ocean.flag[,-which(names(ocean.flag) %in% c("continent"))]
+    ocean.flag$Coord_Units_new <- "WGS84"
+    mapview(hypoxia.dat2) + mapview(ocean.flag,col.region="red")
   
-  
+    
+  # combine flag sites:
+    hypoxia.data.flagged <- rbind(crs.flag,wgs84.flag,USA.out,ocean.flag)
+    saveRDS(hypoxia.data.flagged,"./output/Hypoxia_data_flagged_20190923.rds")
+    
